08fad4c47125cad0369d75a0ced093b837716ee9
Jordan Boyd-Graber <jordanbg@gmail.com>
2017-04-02 10:37:48 -0400

    lstm

---
+++ demos/lstm/loss.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class ToyLossLayer:
+    """
+    Computes square loss with first element of hidden layer array.
+    """
+    @classmethod
+    def loss(self, pred, label):
+        return (pred[0] - label) ** 2
+
+    @classmethod
+    def bottom_diff(self, pred, label):
+        diff = np.zeros_like(pred)
+        diff[0] = 2 * (pred[0] - label)
+        return diff
